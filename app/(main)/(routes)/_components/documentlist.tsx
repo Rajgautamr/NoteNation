16b15d7634badaa3ff4388bbe6c442b2687a3932
rajgautamr <rajgautamr0307@gmail.com>
2024-08-13 23:19:32 +0530

    doc props created

--- app/(main)/(routes)/_components/documentlist.tsx
+++ app/(main)/(routes)/_components/documentlist.tsx
@@ -1,4 +1,19 @@
-const DocumentList = () => {
+"use client"
+
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { useParams, useRouter } from "next/navigation";
+
+interface DocumentListProps{
+    parentdocumentId?: ID<"documents">;
+    level?: number;
+    data?: Doc<"documents"> 
+}
+const DocumentList = ({
+    parentdocumentId,
+    level=0
+}: DocumentListProps) => {
+    const params = useParams();
+    const route = useRouter();
     return ( 
         <div>s</div>
      );
