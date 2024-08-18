4dbc08c69cdc7a1ae700fcd354a45f943132fbbe
rajgautamr <rajgautamr0307@gmail.com>
2024-08-18 23:51:10 +0530

    Expand funt

--- app/(main)/(routes)/_components/documentlist.tsx
+++ app/(main)/(routes)/_components/documentlist.tsx
@@ -2,6 +2,7 @@
 
 import { Doc, Id } from "@/convex/_generated/dataModel";
 import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface DocumentListProps{
     parentdocumentId?: ID<"documents">;
@@ -14,6 +15,15 @@ const DocumentList = ({
 }: DocumentListProps) => {
     const params = useParams();
     const route = useRouter();
+    const [expanded , setExpanded] = useState<Record<string , boolean>>({});
+    const onexpand =(documentId: string) =>{
+        setExpanded(prevExpanded =>({
+            ...prevExpanded,
+            [documentId]:!prevExpanded[documentId]
+        }));
+    };
+
+    const document  
     return ( 
         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quo quidem pariatur eaque qui tempore numquam cumque rerum, odio maiores?</div>
      );
